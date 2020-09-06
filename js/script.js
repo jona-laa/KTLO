38c1f86094cf041fae97973dc3b39a9baad44b44
Jonathan Laasonen <iphntm@iPHNTMs-MBP.lan>
2020-09-06 21:48:11 +0200

    fix flex-items align issue

--- js/script.js
+++ js/script.js
@@ -55,3 +55,17 @@ const sendClick = (e) => {
     e.preventDefault();
     alert('This just a dummy site, Bud. Have a nice day! :)');
 }
+
+//Smooth scrolling
+$('#navbar a, .btn').on('click', function (e) {
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top - 100,
+        },
+            800
+        );
+    }
+});
