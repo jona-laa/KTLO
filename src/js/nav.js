ec25af8ccec85c058bc67cdfc0bd050681ff4c95
Jonathan Laasonen <iphntm@iPHNTMs-MBP.lan>
2020-09-17 00:55:22 +0200

    refactor -ES6ify everything.

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,34 +1,60 @@
-// Toggle mobile menu
-$('#main-menu-toggle').click(function () {
-  $('.main-menu ul').slideToggle(300, function () {
-  });
-});
+// DOM element selectors
+const navbar = document.querySelector('#navbar');
+const toTopBtn = document.querySelector('#goTop');
+const mainMenu = document.querySelector('#menu-main-menu');
 
-window.onscroll = function () {
-  hideMainMenu();
+
+// Hide header & to top on scroll
+window.onscroll = () => {
+  hasScrolled();
   hideToTopBtn();
-}
+};
 
-// Hide Header on on scroll down
-var prevScrollpos = window.pageYOffset;
-const hideMainMenu = () => {
-  var currentScrollPos = window.pageYOffset;
+
+/* Toggle element from top or bottom
+ * @param   {DOM element}   element     Target DOM element to toggle
+ * @param   {string}        position    'top' or 'bottom'
+ * @param   {string}        offset      Offset in e.g. pixels, rem, em, etc.
+*/
+const elementToggle = (element, position, offset) => position === 'top' ? element.style.top = offset : element.style.bottom = offset;
+
+
+/* Change display attribute of element
+ * @param   {DOM element}   element     Target DOM element
+ * @param   {string}        value       Display attribute value, e.g. 'none', 'block', etc.
+*/
+const elementDisplay = (element, value) => element.style.display = value;
+
+
+// Hide Header
+let prevScrollpos = window.pageYOffset;
+
+const hasScrolled = () => {
+  let currentScrollPos = window.pageYOffset;
 
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-    $('.main-menu ul').css('display', 'none')
+    elementToggle(navbar, 'top', '0');
+    elementDisplay(mainMenu, 'none');
   } else {
-    document.getElementById("navbar").style.top = "-125px";
-    $('.main-menu ul').css('display', 'none')
+    elementToggle(navbar, 'top', '-125px');
+    elementDisplay(mainMenu, 'none');
   }
 
   prevScrollpos = currentScrollPos;
 }
 
-// Hide "go to top button" if scrolled down more than window height
+
+// Hide "to top button"
 const hideToTopBtn = () => {
   window.pageYOffset > window.screen.height ?
-    document.getElementById("goTop").style.bottom = "10px"
+    elementToggle(toTopBtn, 'bottom', '10px')
     :
-    document.getElementById("goTop").style.bottom = "-50px";
+    elementToggle(toTopBtn, 'bottom', '-50px')
 }
+
+
+// Toggle mobile menu
+$('#main-menu-toggle').click(function () {
+  $('.main-menu ul').slideToggle(300, function () {
+  });
+});
